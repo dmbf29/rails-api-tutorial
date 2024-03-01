00443fc6b30c4750ea988ae011d66872f2b06e82
Douglas Berkley <douglasmberkley@gmail.com>
2024-03-01 15:57:21 +0900

    added criteria route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :cafes, only: [ :index, :create ]
+      resources :criteria, only: [ :index ]
     end
   end
 end
