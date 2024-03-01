f2498091cc012891ef7bac53bd2ed2f0b7e27330
Douglas Berkley <douglasmberkley@gmail.com>
2024-03-01 16:22:08 +0900

    added cafes#create action

--- app/controllers/api/v1/cafes_controller.rb
+++ app/controllers/api/v1/cafes_controller.rb
@@ -7,4 +7,19 @@ class Api::V1::CafesController < ApplicationController
     end
     render json: @cafes
   end
+
+  def create
+    @cafe = Cafe.new(cafe_params)
+    if @cafe.save
+      render json: @cafe
+    else
+      render json: { error: @cafe.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def cafe_params
+    params.require(:cafe).permit(:title, :address, :picture, :address, :hours, criteria: [])
+  end
 end
